01e2ed9d5f8d6c780aa64827f7bbdd93458bf0a0
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-03-17 14:31:38 +0100

    normalement là c'est bon pour la création de l'arbre
    (on peut meme ajouter des paramètres, ou changer des trucs ça marche nickel chrome)
    et en plus on peut choisir debug=False pour pas avoir tous les print de test ;)

--- decision.py
+++ decision.py
@@ -2,17 +2,20 @@ import csv
 import math 
 import json
 
+global debug
+debug = False
+
 def construire_arbre(data,donnees_possibles,attribut_classe='class',racine={}):
     gains = calcul_gains(data,donnees_possibles,attribut_classe)
     meilleur_attribut = max(gains, key=gains.get, default=None)
-    print(f"meilleur attribut : {meilleur_attribut}")
+    print(f"meilleur attribut : {meilleur_attribut}") if debug else None
 
     if gains[meilleur_attribut] == 0:
         # si le gain est nul, on a atteint une feuille, on renvoie la valeur de la classe
-        print(f"\033[32m\033[1m\033[31mLe gain est nul\033[0m")
-        print('\n')
-        print(data)
-        print(data[0][attribut_classe])
+        print(f"\033[32m\033[1m\033[31mLe gain est nul\033[0m") if debug else None
+        print('\n' if debug else '',end='')
+        print(data if debug else '',end='')
+        print(data[0][attribut_classe] if debug else '',end='')
         racine = {attribut_classe:data[0][attribut_classe]}
         return racine
     racine = {meilleur_attribut:{}}
@@ -20,33 +23,35 @@ def construire_arbre(data,donnees_possibles,attribut_classe='class',racine={}):
     for valeur in donnees_possibles[meilleur_attribut]:
         racine[meilleur_attribut][valeur] = {}
 
-    print(f"\033[32m\033[1marbre créé avec la racine {meilleur_attribut}\033[0m")
+    print(f"\033[32m\033[1marbre créé avec la racine {meilleur_attribut}\033[0m") if debug else None
     afficher_arbre(racine)
-    print('\n\n')
+    print('\n\n' if debug else '',end='')
+
+    if meilleur_attribut=="temp":   # pour le debug
+        pass
+    print(donnees_possibles[meilleur_attribut] if debug else '',end='')
 
     for valeur in donnees_possibles[meilleur_attribut]:
-        print(f"\033[32mtraitement de la valeur \033[1m{valeur}\033[0m\033[32m de l'attribut {meilleur_attribut}\033[0m")
+        print(f"\033[32mtraitement de la valeur \033[1m{valeur}\033[0m\033[32m de l'attribut {meilleur_attribut}\033[0m") if debug else None
         sous_ensemble = [instance for instance in data if instance[meilleur_attribut] == valeur]
-        print(f"\033[35msous-ensemble : {sous_ensemble}\033[0m")
-        print(f"\033[34mdonnées possibles : {donnees_possibles}\033[0m")
-        print('\n')
+        print(f"\033[35msous-ensemble : {sous_ensemble}\033[0m") if debug else None
+        print(f"\033[34mdonnées possibles : {donnees_possibles}\033[0m") if debug else None
+        print('\n' if debug else '',end='')
 
         if len(sous_ensemble)==0:
-            print(f"\033[32m\033[1m\033[31mLe sous-ensemble est vide\033[0m")
-            print('\n')
-            continue
+            print(f"\033[32m\033[1m\033[31mLe sous-ensemble est vide\033[0m") if debug else None
+            print('\n' if debug else '',end='')
+            racine[meilleur_attribut][valeur] = {attribut_classe:'null'}
         elif len(sous_ensemble)==1:
-            print(f"\033[32m\033[1m\033[31mLe sous-ensemble ne contient qu'une seule instance\033[0m")
-            print('\n')
-            racine[meilleur_attribut][valeur] = list(sous_ensemble[0].values())[0]
-            continue
+            print(f"\033[32m\033[1m\033[31mLe sous-ensemble ne contient qu'une seule instance\033[0m") if debug else None
+            print('\n' if debug else '',end='')
+            racine[meilleur_attribut][valeur] = {attribut_classe:sous_ensemble[0][attribut_classe]}
         elif len(donnees_possibles)==1:
-            print(f"\033[32m\033[1m\033[31mIl n'y a plus d'attribut à traiter\033[0m")
-            print('\n')
-            racine[meilleur_attribut][valeur] = list(sous_ensemble[0].values())[0]
-            continue
+            print(f"\033[32m\033[1m\033[31mIl n'y a plus d'attribut à traiter\033[0m") if debug else None
+            print('\n' if debug else '',end='')
+            racine[meilleur_attribut][valeur] = {attribut_classe:max(sous_ensemble, key=sous_ensemble.count)[attribut_classe]}
         else:
-            print(f"\033[32m\033[1m\033[31mIl y a encore des attributs à traiter\033[0m")
+            print(f"\033[32m\033[1m\033[31mIl y a encore des attributs à traiter\033[0m") if debug else None
             # créer un sous-ensemble de données sans l'attribut déjà traité
             sous_ensemble_plus_petit = []
             donnees_possibles_plus_petit = donnees_possibles.copy()
@@ -57,9 +62,9 @@ def construire_arbre(data,donnees_possibles,attribut_classe='class',racine={}):
             for instance in donnees_possibles:
                 if instance == meilleur_attribut:
                     donnees_possibles_plus_petit.pop(instance)
-            print(f"\033[35msous-ensemble plus petit : {sous_ensemble_plus_petit}\033[0m")
-            print(f"\033[34mdonnées possibles plus petites : {donnees_possibles_plus_petit}\033[0m")
-            print(f"\033[33m{racine}\033[0m")
+            print(f"\033[35msous-ensemble plus petit : {sous_ensemble_plus_petit}\033[0m") if debug else None
+            print(f"\033[34mdonnées possibles plus petites : {donnees_possibles_plus_petit}\033[0m") if debug else None
+            print(f"\033[33m{racine}\033[0m") if debug else None
             racine[meilleur_attribut][valeur] = construire_arbre(sous_ensemble_plus_petit,donnees_possibles_plus_petit,attribut_classe)
     afficher_arbre(racine)
     return racine    
@@ -68,15 +73,16 @@ def afficher_arbre(racine,indent=0,debug=False):
     if debug:
         pass
     if indent == 0:
-        print('\033[32m',end='')
+        print('\nArbre construit :') if debug else None
+        print('\033[32m',end='') if debug else None
     for cle,valeur in racine.items():
-        print(f"{'| '*indent}{cle}")
+        print(f"{'| '*indent}{cle}") if debug else None
         if type(valeur) is dict:
-            afficher_arbre(valeur,indent+1)
+            afficher_arbre(valeur,indent+1,debug)
         else:
-            print(f"{'| '*(indent+1)}\033[31m{valeur}\033[0m\033[32m")
+            print(f"{'| '*(indent+1)}\033[31m{valeur}\033[0m\033[32m") if debug else None
     if indent == 0:
-        print('\033[0m')
+        print('\033[0m') if debug else None
 
     with open('tree.json', 'w') as f:
         json.dump(racine, f, indent=4)
@@ -131,7 +137,7 @@ def calcul_gains(data,donnees_possibles,attribut_classe='class'):
     calcul du gain d'information pour un attribut avec les formules de la doc
     """
     gains={}
-    print(donnees_possibles)
+    print(donnees_possibles if debug else '',end='')
     for attribut in donnees_possibles:
         if attribut != attribut_classe:
             p = 0
@@ -143,20 +149,23 @@ def calcul_gains(data,donnees_possibles,attribut_classe='class'):
                 else:
                     n += 1
             gain = I(p,n) - E(data,donnees_possibles,attribut,attribut_classe)
-            print(f"gain({attribut})\t= {(round(gain,10))}")
+            print(f"gain({attribut})\t= {(round(gain,10))}") if debug else None
             gains[attribut] = gain
     return gains
 
 
 filename = "data/golf.csv"
-for instance in read_data(filename)[0]:
-    print(instance)
-print('\n')
-print(read_data(filename)[1])
 
-print('\n')
+# for instance in read_data(filename)[0]:
+#     print(instance)
+# print('\n')
+# print(read_data(filename)[1])
+# print('\n')
+
 attribut_classe = 'play'
 data,donnees_possibles = read_data(filename)
 arbre=construire_arbre(data,donnees_possibles,attribut_classe)
-print('\nArbre construit :')
+
+
+debug = False
 afficher_arbre(arbre,debug=True)
