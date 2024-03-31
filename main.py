597528cedd1c956a30d58161d5771435ac0449fd
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-03-31 16:58:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # Creation et affichage de l'arbre de décision à partir des données d'entrainement (10 premières instances)
     data,donnees_possibles = read_data(filename)
     data_training = data[:10]
-    arbre=construire_arbre(data_training,donnees_possibles,attribut_classe,method="ID3",)
+    arbre=construire_arbre(data_training,donnees_possibles,attribut_classe,method="ID3")
     afficher_arbre(arbre,debug=True)
 
     # Creation et affichage de la matrice de confusion à partir des données de test (Dernières instances)
